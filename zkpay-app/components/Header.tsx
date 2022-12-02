621d23063fbaf241553c58a6e9496e6eab5ac43c
luka1104 <skyward.luke11@gmail.com>
2022-12-02 23:52:17 +0530

    arrange theme

--- zkpay-app/components/Header.tsx
+++ zkpay-app/components/Header.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import { Box, Center, Button, Icon, Text, Link } from '@chakra-ui/react';
+import { Box, Center, Button, Icon, Text, Link, useColorMode } from '@chakra-ui/react';
 import Head from 'next/head';
 import NextLink from 'next/link';
-import { AiOutlinePlus } from 'react-icons/ai';
+import { FiSun, FiMoon } from 'react-icons/fi';
 import { BiWallet } from 'react-icons/bi';
 import { useAccount, useConnect, useDisconnect } from 'wagmi';
 import { InjectedConnector } from 'wagmi/connectors/injected';
@@ -10,6 +10,7 @@ import { shortenEOAName } from 'utils/shortenEOA';
 
 const Header: React.FC = () => {
   const { address, isConnected } = useAccount();
+  const { colorMode, toggleColorMode } = useColorMode()
   const { connect } = useConnect({
     connector: new InjectedConnector(),
   });
@@ -21,9 +22,9 @@ const Header: React.FC = () => {
         <title>ZKJob | Job matching platform</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <Box w="100%" h="60px" p="0 30px 0 0" bg="#FFFDF6">
+      <Box w="100%" h="60px" p="0 30px 0 0" borderBottom='1px solid' borderColor={colorMode === 'light' ? '#F5F5F5' : '#1A202C'}>
         <Center h="100%" justifyContent="space-between">
-          <Link color='black' fontWeight='bold' fontSize='30px' pl='20px' href='/' _hover={{textDecoration: 'none'}}>
+          <Link fontWeight='bold' fontSize='30px' pl='20px' href='/' _hover={{textDecoration: 'none'}}>
             ZKJob
           </Link>
           <Box>
@@ -32,6 +33,9 @@ const Header: React.FC = () => {
                 <Icon as={AiOutlinePlus} />
               </Button>
             </NextLink> */}
+            <Button mr="20px" colorScheme="orange" onClick={toggleColorMode}>
+              {colorMode === 'light' ? (<Icon as={FiMoon} />) : (<Icon as={FiSun} />) }
+            </Button>
             {isConnected ? (
               <Button
                 leftIcon={<BiWallet />}
---
+++ zkpay-app/pages/create.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next'
+import React from 'react'
+
+const create: NextPage = () => {
+  return (
+    <>
+      create
+    </>
+  )
+}
+
+export default create
