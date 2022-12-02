294eb2a9294551f70f6b59f6d76075bd7754a9d7
luka1104 <skyward.luke11@gmail.com>
2022-12-02 21:22:44 +0530

    integrate prisma

---
+++ zkpay-app/prisma/migrations/20221202155204_init/migration.sql
@@ -0,0 +1,29 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "address" TEXT NOT NULL,
+    "nickname" TEXT NOT NULL,
+    "isCompany" BOOLEAN NOT NULL DEFAULT false,
+    "description" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "IntroCard" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "imagePaths" TEXT[],
+    "videoPath" TEXT NOT NULL,
+
+    CONSTRAINT "IntroCard_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_address_key" ON "User"("address");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "IntroCard_userId_key" ON "IntroCard"("userId");
+
+-- AddForeignKey
+ALTER TABLE "IntroCard" ADD CONSTRAINT "IntroCard_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
