8615a665ac119386322e0e66ad30fc8ecbc41354
shun <funakki0904@gmail.com>
2022-12-04 03:20:20 +0530

    "Huddle add Push Chat"

--- zkpay-app/pages/huddle.tsx
+++ zkpay-app/pages/huddle.tsx
@@ -8,13 +8,15 @@ import {
   HuddleClientMethodName,
 } from "@huddle01/huddle01-iframe";
 
+import { Chat } from "@pushprotocol/uiweb";
+
 function Huddle() {
   const [walletAddress, setWalletAddress] = useState("");
 
   const iframeConfig: IframeConfig = {
     roomUrl: "https://iframe.huddle01.com/test-room",
-    height: "600px",
-    width: "80%",
+    height: "580px",
+    width: "100%",
   };
 
   const reactions = [
@@ -48,6 +50,12 @@ function Huddle() {
     <div className="App">
       <div className="container">
         <div>
+          <Chat
+            account="0x94feEEDFcd7ad9a255FE205037c6Df14a8960D3D" //user address
+            supportAddress="0x2Ad2D903FC5f0f28Baa62aD5A377c05a2c07Bab9" //support address
+            apiKey="jVPMCRom1B.iDRMswdehJG7NpHDiECIHwYMMv6k2KzkPJscFIDyW8TtSnk4blYnGa8DIkfuacU0"
+            env="staging"
+          />
           <br />
 
           {Object.keys(huddleIframeApp.methods)
